73ddfbc516c9091071676eb1cdb905b5452d3423
arjun12346 <arjunbhai1088@gmail.com>
2026-01-18 06:35:13 +0530

    Time: 3 ms (79.29%) | Memory: 13.7 MB (63.37%) - LeetSync

--- 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
@@ -3,9 +3,7 @@ public:
     string removeDuplicates(string s) {
        string result;
        for (char c : s) {
-        //if there is some elements already in the bag then chech it is equal with its previous one or not
         if (!result.empty() && result.back() == c) result.pop_back();
-        // if nothing in the array means empty and then add it
         else result.push_back(c);
        } 
        return result;
