eb148c0f7e077aa1c836d281d2660d8908fa2d32
arjun12346 <arjunbhai1088@gmail.com>
2026-01-18 06:34:52 +0530

    Time: 3 ms (79.29%) | Memory: 13.6 MB (63.37%) - LeetSync

--- 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     string removeDuplicates(string s) {
-        string bag = "";
-        for (char c : s) {
-            if (!bag.empty() && bag.back() == c) {
-                bag.pop_back();
-            } else {
-                bag.push_back(c);
-            }
-        }
-        return bag;
+       string result;
+       for (char c : s) {
+        //if there is some elements already in the bag then chech it is equal with its previous one or not
+        if (!result.empty() && result.back() == c) result.pop_back();
+        // if nothing in the array means empty and then add it
+        else result.push_back(c);
+       } 
+       return result;
     }
 };
