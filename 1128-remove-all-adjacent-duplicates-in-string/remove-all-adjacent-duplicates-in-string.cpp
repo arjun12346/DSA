a7b764c657c21f6bee1bfb4bb8586228ab74ca4d
arjun12346 <arjunbhai1088@gmail.com>
2026-01-18 06:32:54 +0530

    Time: 6 ms (52.00%) | Memory: 13.5 MB (87.44%) - LeetSync

--- 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
@@ -1,11 +1,14 @@
 class Solution {
 public:
     string removeDuplicates(string s) {
-       string result;
-       for (char c : s) {
-        if (!result.empty() && result.back() == c) result.pop_back();
-        else result.push_back(c);
-       } 
-       return result;
+        string bag = "";
+        for (char c : s) {
+            if (!bag.empty() && bag.back() == c) {
+                bag.pop_back();
+            } else {
+                bag.push_back(c);
+            }
+        }
+        return bag;
     }
 };
