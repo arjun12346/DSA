ce780feb6e78f70be15aa8c71a007bdb40c78393
arjun12346 <arjunbhai1088@gmail.com>
2026-04-08 18:44:33 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (40.30%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,16 +1,14 @@
 class Solution {
-    // global array
-    static int[] dp;
-    public int getfib(int n) {
+    public int getfib(int n, int[] dp) {
         // base case
         if (n <= 1) return n;
-        if (dp[n] != 0) return dp[n];
-        int ans = getfib(n-1) + getfib(n-2);
+        if (dp[n] != 0) return dp[n]; // present in dp array or not
+        int ans = getfib(n-1, dp) + getfib(n-2, dp);
         dp[n] = ans;
         return ans;
     }
     public int fib(int n) {
-        dp = new int[n+1];
-        return getfib(n);
+        int[] dp = new int[n+1]; // index from 0 to 1
+        return getfib(n, dp);
     }
 }
