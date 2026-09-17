8add8e638a097a34957bcc496f20577135523c50
arjun12346 <arjunbhai1088@gmail.com>
2026-09-17 10:20:38 +0530

    Time: 12 ms (73.43%) | Memory: 48 MB (33.74%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -3,21 +3,21 @@ class Solution {
         int n = s.length();
         int left = 0;
         int right = 0;
-        int maxLength = 0;
+        int count = 0;
         int freq[] = new int[128];
+        int maxi = 0;
         while (right < n) {
             char ch = s.charAt(right);
             freq[ch]++;
-            // duplicates found
+            count++;
             while (freq[ch] > 1) {
                 freq[s.charAt(left)]--;
+                count--;
                 left++;
             }
-            //current window is valid
-            maxLength = Math.max(maxLength, right - left + 1);
+            maxi = Math.max(maxi, count);
             right++;
-
         }
-        return maxLength;
+        return maxi;
     }
 }
